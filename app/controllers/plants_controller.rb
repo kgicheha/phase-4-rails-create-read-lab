f9e312646603293a4c83cd668cc5584e9390c039
Kevin Gicheha <gichehakevin@gmail.com>
2022-08-29 14:39:33 -0700

    commit tree d0d85dc4deb12ae33ed6f7f98b3b68f7af3bfc62

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,27 @@
+class PlantsController < ApplicationController
+    wrap_parameters format: []
+
+    def index
+        render json: Plant.all, status: :ok
+    end
+
+    def show
+        plants = Plant.find_by(id: params[:id])
+        if plants
+            render json: plants, status: :ok
+        else
+            render json: {error: "production not found"}, status: :not_found
+        end
+    end
+
+    def create
+        plants = Plant.create(plants_params)
+        render json: plants, status: :created
+    end
+
+    private
+    def plants_params
+        params.permit(:name, :image, :price)
+    end
+
+end
